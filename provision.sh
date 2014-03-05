963efbcb4476202604422d3ecb9e9f8132883598
Adam Davies <adzdavies@gmail.com>
2014-03-05 11:56:00 +1030

    Do an upgrade to avoid issues

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 # Install firefox
 apt-get update
+apt-get upgrade -y
 apt-get install firefox lxde lxdm virtualbox-ose-guest-x11 -y
 
 # This folder is shared as /vagrant inside the virtual box
